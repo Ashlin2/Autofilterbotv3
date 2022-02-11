a0a0a6c7fbbb6286116dc9886b8194f63bfac4bb
Ashlin2 <90307567+Ashlin2@users.noreply.github.com>
2022-02-11 06:20:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Autofilterbotv3 is a open source project. 
+- Autofilterbotv3 is a open source project... 
 - Source - https://github.com/Ashlin2/Autofilterbotv3  
 
 <b>DEVS:</b>
